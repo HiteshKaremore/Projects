af6da779859e227a22b7cdf6f7c4694f4769a9ad
Hitesh Karemore <reachitesh0208@gmail.com>
2026-02-17 10:50:10 +0530

    Add habit creation logic with input validation and state management

--- habitTracker/app.js
+++ habitTracker/app.js
@@ -4,4 +4,12 @@ let inputElement = document.getElementById("input-habit");
 let addButton = document.getElementById("add-habit");
 let displayContainer = document.getElementById("habit-display");
 
-
+addButton.addEventListener("click", function () {
+  let val = inputElement.value.trim();
+  if (val === "") {
+    return;
+  }
+  habits.push(val);
+  console.log(habits);
+  inputElement.value = "";
+});
