2cd8adccc57393dca39e5bc3bcec1b177c72d4b9
Hitesh Karemore <reachitesh0208@gmail.com>
2026-02-17 09:35:38 +0530

    added app.js

---
+++ habitTracker/app.js
@@ -0,0 +1,7 @@
+let habits = [];
+
+let inputElement = document.getElementById("input-habit");
+let addButton = document.getElementById("add-habit");
+let displayContainer = document.getElementById("habit-display");
+
+
